e8f0ed7c4e9a1ddae208211a91bae7be6b661850
Mark Batho <mark747488@gmail.com>
2023-06-22 10:17:44 +0200

    Set up eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ['eslint:recommended', 'plugin:react/jsx-runtime', 'prettier'],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  plugins: ['react'],
+  rules: {
+    indent: ['error', 2],
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
+    'react/jsx-uses-react': 'error',
+    'react/jsx-uses-vars': 'error',
+  },
+};
--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
+// import './styles/App.css';
 import './styles/App.css';
 
 function App() {
-  return (
-    <div className="App"></div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
